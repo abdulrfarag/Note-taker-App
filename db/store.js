06d494368d87d210758f47d85f50fbf5642b3c16
abdulrfarag <ar060290@gmail.com>
2023-01-30 18:22:12 -0600

    created a read function

--- db/store.js
+++ db/store.js
@@ -6,3 +6,8 @@ const { v4: generateId } = require('uuid')
 const readFileAsync = util.promisify(fs.readFile)
 const writeFileAsync = util.promisify(fs.writeFile)
 
+class Store {
+
+    read() {
+        return readFileAsync('db/db.json', 'utf8')
+    }
