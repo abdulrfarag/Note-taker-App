0985d5448a0f57b6e5666d751c484628b893a246
abdulrfarag <ar060290@gmail.com>
2023-01-30 18:25:01 -0600

    created the routes apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,22 @@
-const router= require('express').Router()
+const router = require('express').Router()
+const store = require('../db/store')
 
-module.exports= router
+router.get('/notes', (req, res) => {
+    store.getNotes()
+        .then((notes) => { res.json(notes) })
+        .catch((err) => { res.status(500).json(err) })
+})
+
+router.post('/notes', (req, res) => {
+    store.addNote(req.body).then((newNote) => {
+        res.json(newNote)
+    }).catch(err => { res.status(500).json(err) })
+})
+
+router.delete('/notes/:id', (req, res) => {
+    store.deleteNote(req.params.id)
+        .then(() => { res.json({ 'ok': "note successfully deleted" }) })
+        .catch(err=>{res.status(500).json(err)})
+})
+
+module.exports = router
